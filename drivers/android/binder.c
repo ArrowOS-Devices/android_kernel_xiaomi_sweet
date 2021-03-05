09a1bf8832f9149fa0e8faabae4de731e6dc7234
bsp-open <bsp-open@xiaomi.com>
2021-03-05 16:23:48 +0800

    Kernel: Xiaomi kernel changes for Redmi Note 10 Pro Android R
    The Patch based on QualComm release TAG:LA.UM.9.1.r1-06700-SMxxx0.0-1
    The kernel config file used is sweet_user_defconfig
    
    Change-Id: Ic2cfcf12a5398d919dcb03d4d8a21530e5174ba7
    Signed-off-by: bsp-open <bsp-open@xiaomi.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3,6 +3,7 @@
  * Android IPC Subsystem
  *
  * Copyright (C) 2007-2008 Google, Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -71,7 +72,6 @@
 #include <linux/pid_namespace.h>
 #include <linux/security.h>
 #include <linux/spinlock.h>
-
 #include <uapi/linux/android/binder.h>
 #include <uapi/linux/sched/types.h>
 #include "binder_alloc.h"
@@ -4148,6 +4148,7 @@ static int binder_wait_for_work(struct binder_thread *thread,
 		prepare_to_wait(&thread->wait, &wait, TASK_INTERRUPTIBLE);
 		if (binder_has_work_ilocked(thread, do_proc_work))
 			break;
+
 		if (do_proc_work)
 			list_add(&thread->waiting_thread_node,
 				 &proc->waiting_threads);
